07a30e71e524a089f9ce2f87dd9d0b3291297563
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-08-03 14:43:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ response = requests.get(url)
 print(response.text)
 
 
-print ((response.text).find('Your IP Address is <strong>'))
+
 #use discord webhook and send ip to your server
 print((response.text)[(response.text).find('Your IP Address is <strong>'):(response.text).find('</strong>.</p>')])
